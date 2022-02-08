6acae4c8235c00e272e38be5e3fed209dd8cf079
nix-arif <nix.arif@gmail.com>
2022-02-09 00:18:27 +0800

    Transaction screen

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,6 +1,45 @@
 import React, { useContext } from "react";
 import { TransactionContext } from "../context/TransactionContext";
 import dummyData from "../utils/dummyData";
+import { shortenAddress } from "../utils/shortenAddress";
+
+const TransactionCard = ({
+  addressTo,
+  addressFrom,
+  timestamp,
+  message,
+  keyword,
+  amount,
+  url,
+}) => {
+  return (
+    <div className="bg-[#181918] m-4 flex flex-1 2xl:min-w-[450px] 2xl:max-w-[500px] sm:min-w-[270px] sm:max-w-[300px] flex-col p-3 rounded-md hover:shadow-2xl">
+      <div className="flex flex-col items-center w-full mt-3">
+        <div className="w-full mb-6 p-2">
+          <a
+            href={`https://ropsten.etherscan.io/address/${addressFrom}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="text-white text-base">
+              From: {shortenAddress(addressFrom)}
+            </p>
+          </a>
+          <a
+            href={`https://ropsten.etherscan.io/address/${addressTo}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="text-white text-base">
+              To: {shortenAddress(addressTo)}
+            </p>
+          </a>
+          <p className="text-white text-base">Amount: {amount} ETH</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Transactions = () => {
   const { currentAccount } = useContext(TransactionContext);
@@ -17,6 +56,12 @@ const Transactions = () => {
             Connect your account to see the latest transactions
           </h3>
         )}
+
+        <div className="flex flex-wrap justify-center items-center mt-10">
+          {dummyData.reverse().map((transaction, i) => (
+            <TransactionCard key={i} {...transaction} />
+          ))}
+        </div>
       </div>
     </div>
   );
